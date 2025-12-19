7399b4fa56acfa556bc8a581b1fc30031dff45bb
fuchuanxin <1392532278@qq.com>
2025-12-19 17:40:27 +0800

    docs: 完善DruidProperties类注释

--- src/main/java/com/axin/framework/config/properties/DruidProperties.java
+++ src/main/java/com/axin/framework/config/properties/DruidProperties.java
@@ -1,9 +1,14 @@
 package com.axin.framework.config.properties;
 
 /**
+ * Druid数据源属性配置类
+ * 
+ * <p>用于存储Druid数据源的配置属性，预留用于扩展。</p>
+ * 
+ * <p>当前为空实现，可根据需要添加具体的配置属性。</p>
+ * 
  * @author fuchuanxin
  * @version 1.0
- * @description: TODO
  * @date 2025/9/23 11:04
  */
 public class DruidProperties {
